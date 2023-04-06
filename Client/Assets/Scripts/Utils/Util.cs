01b4fbd5fc45b475320d3e219e64413e43d732ca
Lee Woo-Yeol <dnduf33@gmail.com>
2023-04-06 18:37:56 +0900

    ParseJson 주석
    Json Parsing 함수 주석 추가

--- Client/Assets/Scripts/Utils/Util.cs
+++ Client/Assets/Scripts/Utils/Util.cs
@@ -58,9 +58,23 @@ namespace Client
             return transform.gameObject;
         }
 
-        
+
         /// <summary>
-        /// json 파일 파싱하여 오브젝트로 반환
+        /// json 파일 파싱하여 오브젝트로 반환<br/>
+        /// <br/>
+        /// * 필요 클래스<br/>
+        /// {0} - 실제 데이터 가지고 있는 클래스<br/>
+        /// {0}Handler - member로 이름이 ({0}s).ToLower() 인 List&lt;{0}&gt; 필요<br/>
+        ///<br/>
+        /// * json 파일<br/>
+        /// Assets/Resources/Jsons/{0}s.json<br/>
+        /// <br/>
+        /// ex)<br/>
+        /// {0} = MonsterStat인 경우,<br/>
+        /// <br/>
+        /// class MonsterStat - 데이터 가지고 있는 클래스<br/>
+        /// class MonsterStatHandler { public List&lt;MonsterStat&gt; monsterstats; }<br/>
+        /// Assets/Resources/Jsons/MonsterStats.json
         /// </summary>
         public static Handler ParseJson<Handler>(string path = null, string handle = null)
         {
@@ -71,6 +85,12 @@ namespace Client
 
                 path = string.Concat(name.Substring(0, idx), 's');
                 handle = path.ToLower();
+            }
+            else if (string.IsNullOrEmpty(handle))
+            {
+                string name = typeof(Handler).Name;
+                int idx = name.IndexOf("Handler");
+                handle = string.Concat(name.Substring(0, idx), 's').ToLower();
             }
 
             TextAsset jsonTxt = Resources.Load<TextAsset>($"Jsons/{path}");
