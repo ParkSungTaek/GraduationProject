a368bf624f43fc64ab289108b52ebbd62e0cb638
Lee Woo-Yeol <dnduf33@gmail.com>
2023-10-04 22:28:52 +0900

    개인키 경로 변경

--- Server/z02.sendToLinux.bat
+++ Server/z02.sendToLinux.bat
@@ -8,7 +8,7 @@ if exist psftp.txt (
 echo put Server.zip >> psftp.txt
 echo quit >> psftp.txt
 
-psftp 3.35.30.92 -i E:\hongik_SE.ppk -l ubuntu -b psftp.txt
+psftp 3.35.30.92 -i ..\hongik_SE.ppk -l ubuntu -b psftp.txt
 
 del Server.zip
 del psftp.txt
